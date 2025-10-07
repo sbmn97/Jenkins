08adf144454049848b940e2dfdb575e563e761d1
sbmn97 <sbmn97@yahoo.com>
2025-10-07 11:31:48 +0800

    Initial commit: Jenkins static site project

---
+++ site/app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('cta');
+const msg = document.getElementById('msg');
+button.addEventListener('click', () => {
+  const now = new Date().toLocaleString();
+  msg.textContent = `Hello from Jenkins! (${now})`;
+});
